0c872f009a2b60e3c0784056f3951b146b416f4a
cactusfluo <cactus@ntymail.com>
2018-11-12 14:30:21 +0100

    [-]shop*

--- chaincode/market/f_shopCreate.go
+++ chaincode/market/f_shopCreate.go
@@ -1,11 +1,41 @@
 package main
 
 import	"fmt"
-//import	"encoding/json"
+import	"encoding/json"
 
 ////////////////////////////////////////////////////////////////////////////////
 /// PRIVATE
 ////////////////////////////////////////////////////////////////////////////////
+
+func	getShopStruct(name string, userKey string) ([]byte, error) {
+	var	err			error
+	var	shop		Shop
+	var	bytes		[]byte
+
+	shop.Users = []string{userKey}
+	shop.Name = name
+	bytes, err = json.Marshal(shop)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot marshal basic shop struct. %s", err)
+	}
+	return bytes, nil
+}
+
+func	getUserStruct(userKey string) ([]byte, error) {
+	var	err			error
+	var	user		User
+	var	userString	string
+
+	str, err = _get([]string{userKey})
+	if err != nil {
+		return nil, fmt.Errorf("Cannot get user info. %s", err)
+	}
+	err = json.Unmarshal([]byte(userString), &user)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot get user info. %s", err)
+	}
+	Append(user.Shops, )
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 /// PUBLIC
@@ -14,6 +44,8 @@ import	"fmt"
 func	funcShopCreate(args []string) (string, error) {
 	var	err			error
 	var	publicKey	string
+	var	shop		[]byte
+	var	user		[]byte
 
 	// GET PUBLIC KEY
 	publicKey, err = getPublicKey()
@@ -21,9 +53,14 @@ func	funcShopCreate(args []string) (string, error) {
 		return "", err
 	}
 	// CHECK ARGUMENTS
-	if len(args) != 0 {
-		return "", fmt.Errorf("This function does not require any argument.")
+	if len(args) != 1 {
+		return "", fmt.Errorf("This function requires one argument.")
 	}
-	fmt.Printf("%s", publicKey)
+	// GET SHOP STRUCT
+	shop, err = getShopStruct(args[1], publicKey)
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("%s", shop)
 	return "", nil
 }
--- chaincode/shop_shop/globals.go
+++
@@ -1,6 +0,0 @@
-package	main
-
-import	"github.com/hyperledger/fabric/core/chaincode/shim"
-
-var STUB shim.ChaincodeStubInterface
-var LOG *shim.ChaincodeLogger
